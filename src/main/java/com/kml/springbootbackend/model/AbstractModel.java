fb99e5becdd3e7d9385965966ac6d91d2d86a4b5
Kin Ming Lo <paulkinming@gmail.com>
2025-01-15 13:14:12 +0800

    Replace @Data with @Getter & @Setter

--- src/main/java/com/kml/springbootbackend/model/AbstractModel.java
+++ src/main/java/com/kml/springbootbackend/model/AbstractModel.java
@@ -3,9 +3,11 @@ package com.kml.springbootbackend.model;
 import java.io.Serializable;
 import java.time.Instant;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-// @Data
+@Getter
+@Setter
 public abstract class AbstractModel implements Serializable, Cloneable {
     
     private static final long serialVersionUID = 1L;
