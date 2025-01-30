0a855375d9129d6e647cbfe8308d1c5078deb1b1
PaulKinMingLo <paulkinming@gmail.com>
2025-01-30 16:59:57 +0800

    [JWT Token] Add isValid method

--- src/main/java/com/kml/springbootbackend/service/JWTtokenService.java
+++ src/main/java/com/kml/springbootbackend/service/JWTtokenService.java
@@ -9,12 +9,15 @@ import org.springframework.stereotype.Service;
 
 import com.kml.springbootbackend.model.User;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 @Service
 public class JWTtokenService {
     
     private final SecretKey secretKey;
+    
+    private final String issuer = "kml";
 
     JWTtokenService() {
         this.secretKey = Jwts.SIG.HS256.key().build();
@@ -23,11 +26,25 @@ public class JWTtokenService {
     public String generateToken(User user) throws Exception {
         String jws = Jwts.builder()
             .signWith(secretKey)
-            .issuer("kml")
+            .issuer(issuer)
             .expiration(Date.from(Instant.now().plusSeconds(36000)))
             .issuedAt(new Date())
             .claim("username", user.getUsername()).compact();
 
         return jws;
+    }
+
+    // return true if token is signed with the secret key and not expired
+    public boolean isValid(String token) {
+        Claims claims = Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token).getPayload();
+
+        Instant expDate = claims.get("exp", Date.class).toInstant();
+
+        if (expDate.isBefore(new Date().toInstant()) && claims.get("iss", String.class).equals(issuer)) {
+            return true;
+        }
+        
+        return false;
     }
 }
